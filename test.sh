3cfdc119cff1336c9ae1c1a59fee6ef00ef6f479
Joe Politz <joe@cs.brown.edu>
2023-05-12 14:33:10 -0700

    names

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore ListExamplesTests
