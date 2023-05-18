719eb9b0844b3f6188101705fe48c96f8f0fdd7b
pandrew99 <ajpan@ucsd.edu>
2023-05-18 00:49:04 -0700

    added comment on how to fix bug in ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -40,7 +40,8 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      index1 += 1;
+      // change index1 below to index 2 to fix test
+      index2 += 1;
     }
     return result;
   }
