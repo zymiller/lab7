44239e31bed46dc39c5b64f0783d98b852f1e1c6
Zachary Y Miller <cs15lsp23rx@ieng6-202.ucsd.edu>
2023-06-07 23:15:10 -0700

    Fixed bugs within ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -41,7 +41,7 @@ class ListExamples {
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
       // change index1 below to index2 to fix test
-      index2 += 1;
+      index1 += 1;
     }
     return result;
   }
